151bce7c7a1994d400c8df63c4c60afce38baa2e
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2023-03-28 12:01:07 +0200

    small generics fix

--- src/main/java/org/cardanofoundation/list/HashedList.java
+++ src/main/java/org/cardanofoundation/list/HashedList.java
@@ -8,14 +8,14 @@ import java.util.function.Function;
 
 public record HashedList<T>(List<T> list, byte[] hash) {
 
-    public static <T> HashedList<T> create(List<T> items, Function<T, byte[]> hasher) {
+    public static <T> HashedList<byte[]> create(List<T> items, Function<T, byte[]> hasher) {
         val hashedItems = items.stream().map(hasher).toList();
 
         val zero = Hashing.sha2_256(new byte[0]);
 
         val hash = hashedItems.stream().reduce(zero, Hashing::combineHash);
 
-        return new HashedList(hashedItems, hash);
+        return new HashedList<>(hashedItems, hash);
     }
 
 }
